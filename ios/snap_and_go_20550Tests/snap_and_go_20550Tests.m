f6919367c2c84ea152a94d93ae1512f145dce3c1
Crowdbotics <team@crowdbotics.com>
2020-09-22 04:21:48 +0000

    Initial commit.

---
+++ ios/snap_and_go_20550Tests/snap_and_go_20550Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface snap_and_go_20550 : XCTestCase
+
+@end
+
+@implementation snap_and_go_20550
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
